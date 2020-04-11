e07dbbf35008bcbf3ac6dfcd626056d04605a08c
yingchunbai <953152868@qq.com>
2020-04-11 20:52:27 +0800

    add stack and queue

---
+++ StackAndQueue/线性表/链式线性表/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  链式线性表
+//
+//  Created by mac on 2020/4/11.
+//  Copyright © 2020 mac. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
